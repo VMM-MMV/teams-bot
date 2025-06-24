1f3fb141b37cea0684e68d9a835486e1a243bc29
mihai.vieru <mihai.vieru@planetgroupint.com>
2025-06-24 15:54:11 +0300

    update structure

--- src/app.py
+++ src/app.py
@@ -1,7 +1,3 @@
-"""
-Copyright (c) Microsoft Corporation. All rights reserved.
-Licensed under the MIT License.
-"""
 from http import HTTPStatus
 
 from aiohttp import web
--- src/config.py
+++ src/config.py
@@ -1,8 +1,3 @@
-"""
-Copyright (c) Microsoft Corporation. All rights reserved.
-Licensed under the MIT License.
-"""
-
 import os
 
 from dotenv import load_dotenv
@@ -17,6 +12,3 @@ class Config:
     APP_PASSWORD = os.environ.get("BOT_PASSWORD", "")
     APP_TYPE = os.environ.get("BOT_TYPE", "")
     APP_TENANTID = os.environ.get("BOT_TENANT_ID", "")
-    AZURE_OPENAI_API_KEY = os.environ["AZURE_OPENAI_API_KEY"] # Azure OpenAI API key
-    AZURE_OPENAI_MODEL_DEPLOYMENT_NAME = os.environ["AZURE_OPENAI_MODEL_DEPLOYMENT_NAME"] # Azure OpenAI model deployment name
-    AZURE_OPENAI_ENDPOINT = os.environ["AZURE_OPENAI_ENDPOINT"] # Azure OpenAI endpoint

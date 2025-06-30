cc284bcec7b43d43bb302595983c1caa8b40fc60
mihai.vieru <mihai.vieru@planetgroupint.com>
2025-06-30 12:30:32 +0300

    add profile specific db name

--- src/agent_service.py
+++ src/agent_service.py
@@ -1,11 +1,12 @@
 import os
 import agent.agent as agent
 from db import AsyncChatStore
+from agent.utils.config import config
 
 async def invoke_agent(user_id: str, question: str):
     os.makedirs("db", exist_ok=True)
 
-    async with AsyncChatStore("db/chat.db") as store:
+    async with AsyncChatStore(f"db/{config.db.name}.db") as store:
         user_messages = [x.message for x in await store.get_messages(user_id)]
 
         result = await agent.ainvoke(question, user_messages)
